fa071ffef159aa7cff57ffa767c1396bc97ecf6b
ksm03071 <ksm03071@gmail.com>
2020-06-30 16:39:16 +0900

    FormsAPI 활용하기 및 forms.py 파일추가
    - 기존코드삭제
    - FormsAPI 코드적용
    - forms.py 파일생성

---
+++ rooms/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from . import models
+
+
+class SearchForm(forms.Form):
+
+    city = forms.CharField(initial="AnyWhere")
+    price = forms.IntegerField(required=False)
+    room_type = forms.ModelChoiceField(queryset=models.RoomType.objects.all())
+
