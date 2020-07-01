4b30594b52ad97e45c02a65923b56df11bbbe24c
ksm0207 <ksm03071@naver.com>
2020-07-02 03:56:20 +0900

    HttpResponse 에러 해결완료 및 이메일유효성검사 진행중
    - HttpResponse 에러해결완료
    - 이메일 유효성검사 1차 진행중

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from . import models
 
 
 class LoginForm(forms.Form):
@@ -6,3 +7,14 @@ class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(username=email)
+            return email
+        except models.User.DoesNotExist:
+            raise forms.ValidationError("존재하지 않는 사용자입니다")
+
+    def clean_password(self):
+        return "클린 패스워드"
+        # print("clean_password")
--- users/views.py
+++ users/views.py
@@ -5,10 +5,13 @@ from . import forms
 
 class LoginView(View):
     def get(self, request):
-        form = forms.LoginForm()
+        form = forms.LoginForm(initial={"email": "ksm03071@naver.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
-        print(form)
+        if form.is_valid():
+            print(form.cleaned_data)
+
+        return render(request, "users/login.html", {"form": form})
 
