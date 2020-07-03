91e469894ab0f805c1f354ff9f9a491bb0e07346
ksm0207 <ksm03071@naver.com>
2020-07-04 06:21:27 +0900

    회원가입기능 구현중

--- users/forms.py
+++ users/forms.py
@@ -43,3 +43,14 @@ class SignUpForm(forms.Form):
             raise forms.ValidationError("일치하지 않은 비밀번호 입니다")
         else:
             return password
+
+    def save(self):
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name ")
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user = models.User.objects.create_user(email, email, password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save
+
--- users/views.py
+++ users/views.py
@@ -32,3 +32,11 @@ class SignUpView(FormView):
     success_url = reverse_lazy("core:home")
     initial = {"first_name": "Kim", "last_name": "Sung", "email": "root@naver.com"}
 
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
