724f27f35773af18172e1e81df738f555d762d74
ksm0207 <ksm03071@naver.com>
2020-07-09 04:28:03 +0900

    Github 로그인구현완료
    -  Path 재설정 및 Views.py 코드 줄맞춤 작업완료
    - 유저관리자패널에서 사용자 로그인정보 저장완료
    -

--- users/urls.py
+++ users/urls.py
@@ -5,17 +5,11 @@ app_name = "users"
 
 urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
-    path("login/github", views.SignUpView.github_login, name="github-login"),
-    path(
-        "login/github/callback",
-        views.SignUpView.github_callback,
-        name="github-callback",
-    ),
-    path("logout", views.LoginView.log_out, name="logout"),
+    path("login/github", views.github_login, name="github-login"),
+    path("login/github/callback", views.github_callback, name="github-callback",),
+    path("logout", views.log_out, name="logout"),
     path("signup", views.SignUpView.as_view(), name="signup"),
     path(
-        "verify/<str:key>",
-        views.SignUpView.complete_verification,
-        name="complete-verification",
+        "verify/<str:key>", views.complete_verification, name="complete-verification",
     ),
 ]
