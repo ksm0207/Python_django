621561d96ff8da25fca1583eca2803b863171b2e
ksm0207 <ksm03071@naver.com>
2020-07-10 04:19:44 +0900

    카카오이미지 User panel --> avatar 저장완료
    - 카카오이미지 User panel에 저장완료

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
+from django.core.files.base import ContentFile
 from . import forms, models
 
 
@@ -164,7 +165,8 @@ def kakao_callback(request):
             raise KakaoException()
         profile = kakao_account.get("profile")
         nickname = profile.get("nickname")
-
+        profile_image = profile.get("profile_image_url")
+        print(profile_image)
         try:
             user = models.User.objects.get(email=email)
             if user.login_method != models.User.LOGIN_KAKAO:
@@ -179,6 +181,11 @@ def kakao_callback(request):
             )
             user.set_unusable_password()
             user.save()
+            if profile_image is not None:
+                photo_request = requests.get(profile_image)
+                user.avatar.save(
+                    f"{nickname}-avatar", ContentFile(photo_request.content)
+                )
         login(request, user)
         return redirect(reverse("core:home"))
 
