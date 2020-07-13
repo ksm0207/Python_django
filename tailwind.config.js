849f113fffa93802dcf23e36aae70d8952c07e40
ksm0207 <ksm03071@naver.com>
2020-07-14 08:52:26 +0900

    홈페이지 가운데 화면만들기 완료
    - vh Classname 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  purge: [],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '25vh': '25vh',
+        '50vh': '50vh',
+        '60vh': '60vh',
+        '75vh': '75vh',
+      },
+      borderRadius: {
+        xl: '1.5rem',
+      },
+    },
   },
   variants: {},
   plugins: [],
