a042c0b624c0d8313574e3eeeecf9f7ed95f394a
ksm0207 <ksm03071@naver.com>
2020-07-25 02:33:20 +0900

    방 수정을 위한 View 파일 완성
    - None

--- rooms/views.py
+++ rooms/views.py
@@ -140,11 +140,11 @@ class EditRoomView(user_mixins.LoggedInOnlyView, UpdateView):
 class RoomPhotosView(user_mixins.LoggedInOnlyView, DetailView):
 
     model = models.Room
-    template_name = "room_photos.html"
+    template_name = "rooms/room_photos.html"
 
     def get_object(self, queryset=None):
         room = super().get_object(queryset=queryset)
         if room.host.pk != self.request.user.pk:
             raise Http404()
-            return room
+        return room
 
