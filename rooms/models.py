f9de333f8a04e1e78ea95af78d5cc2305f7d7c3a
ksm0207 <ksm03071@naver.com>
2020-07-29 13:20:25 +0900

    12월 --> 1월로 넘어갈때 연도+1 로직추가
    - None

--- rooms/models.py
+++ rooms/models.py
@@ -128,8 +128,11 @@ class Room(core_models.TimeStampedModel):
         this_year = now.year
         this_month = now.month
         next_month = this_month + 1
+        next_year = this_year
         if this_month == 12:
             next_month = 1
+            next_year = this_year + 1
+
         this_month_cal = Calendar(this_year, this_month)
-        next_month_cal = Calendar(this_year, next_month)
+        next_month_cal = Calendar(next_year, next_month)
         return [this_month_cal, next_month_cal]
