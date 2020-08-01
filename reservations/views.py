137c73b154c9db1ac53a0427153883cd81806619
ksm0207 <ksm03071@naver.com>
2020-08-01 16:59:37 +0900

    사용자 예약 화면 작업중
    - 페이지를 요청하는 user는 게스트 이거나 호스트 이어야함
    - 호스트 이거나 게스트 도 아닐시 http404 에러발생
    - 예약 화면 작업중

--- reservations/views.py
+++ reservations/views.py
@@ -1,6 +1,7 @@
 import datetime
+from django.http import Http404
 from django.contrib import messages
-from django.shortcuts import redirect, reverse
+from django.shortcuts import render, redirect, reverse
 from rooms import models as room_models
 from django.views.generic import View
 from . import models
@@ -32,8 +33,14 @@ def create(request, room, year, month, day):
 
 
 class ReservationDetailView(View):
-    def get(self, pk):
+    def get(self, *args, **kwargs):
+        pk = kwargs.get("pk")
         reservation = models.Reservation.objects.get_or_none(pk=pk)
-        if not reservation:
-            return redirect(reverse("core:home"))
-
+        if not reservation or (
+            reservation.guest != self.request.user
+            and reservation.room.host != self.request.user
+        ):
+            raise Http404()
+        return render(
+            self.request, "reservations/detail.html", {"reservation": reservation}
+        )
