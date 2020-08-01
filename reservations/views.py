b8db624dcbdb0621635868305a0129302b04aa80
ksm0207 <ksm03071@naver.com>
2020-08-01 10:48:42 +0900

    사용자 예약 기능 구현중
    - None

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import datetime
+from django.contrib import messages
+from django.shortcuts import render, redirect, reverse
+from rooms import models as room_models
+from django.views.generic import View
+from . import models
 
-# Create your views here.
+
+class CreateError(Exception):
+    pass
+
+
+def create(request, room, year, month, day):
+
+    try:
+        date_obj = datetime.datetime(year, month, day)
+        room = room_models.Room.objects.get(pk=room)
+        models.BookedDay.objects.get(day=date_obj, reservation__room=room)
+        raise CreateError()
+    except (room_models.Room.DoesNotExist, CreateError):
+        messages.error(request, "선택하신 방은 예약 할수 없는 방 입니다.")
+        return redirect(reverse("core:home"))
+
+    except models.BookedDay.DoesNotExist:
+        reservation = models.Reservation.objects.create(
+            guest=request.user,
+            room=room,
+            check_in=date_obj,
+            check_out=date_obj + datetime.timedelta(days=1),
+        )
+        return redirect(reverse("reservations:detail", kwargs={"pk": reservation.pk}))
+
+
+class ReservationDetailView(View):
+    def get(self):
+        pass
+
