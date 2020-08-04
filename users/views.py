c62d8799cdbf63ec3e1e58bd1aa089ac19993a4f
ksm0207 <ksm03071@naver.com>
2020-08-04 14:11:10 +0900

    검색바 / 로그인 / 회원가입 / Footer / 번역 완료
    - 한국어
    - 스페인어
    - 영어
    - 일본어
    부분별 번역 구현 완료

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from django.utils import translation
 from django.http import HttpResponse
 from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
@@ -11,6 +12,7 @@ from django.contrib.auth.views import PasswordChangeView
 from django.contrib.messages.views import SuccessMessageMixin
 from . import forms, models, mixins
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
 
 class LoginView(mixins.LoggedOutOnlyView, FormView):
@@ -277,6 +279,7 @@ def switch_hosting(request):
 
 def switch_language(request):
     lang = request.GET.get("lang", None)
-    if lang is not None:
-        pass
-    return HttpResponse(status=200)
+    translation.activate(lang)
+    response = HttpResponse(status=200)
+    response.set_cookie(settings.LANGUAGE_COOKIE_NAME, lang)
+    return response
