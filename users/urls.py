72ed4cbe199de8ea6ae42a9e45b69ea4ccf424c3
ksm0207 <ksm03071@naver.com>
2020-08-04 11:34:36 +0900

    언어번역 테스트중
    - Select CSS 변경
    - 언어번역 테스트중

--- users/urls.py
+++ users/urls.py
@@ -18,4 +18,5 @@ urlpatterns = [
     path("update-password/", views.UpdatePasswordView.as_view(), name="password"),
     path("switch-hosting/", views.switch_hosting, name="switch-hosting"),
     path("<int:pk>/", views.UserProfileView.as_view(), name="profile"),
+    path("switch-language/", views.switch_language, name="switch-language"),
 ]
