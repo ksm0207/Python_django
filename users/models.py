41c92809ce11114618861aaa41ed6acd1fb86009
ksm0207 <ksm03071@naver.com>
2020-08-14 13:18:14 +0900

    구문 수정
    - None

--- users/models.py
+++ users/models.py
@@ -74,7 +74,7 @@ class User(AbstractUser):
         max_length=3,
         default=CURRENCY_KRW,
     )
-    
+    objects = core_managers.CustomModelManager()
     superhost = models.BooleanField(default="False")
     email_verified = models.BooleanField(default=True)
     email_secret = models.CharField(max_length=20, default="", blank=True)
